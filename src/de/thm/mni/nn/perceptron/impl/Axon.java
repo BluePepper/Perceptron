a07b0143415d5c4d25e6a605e560e23b10d300be
Marcel Walden <Marcel.Walden@me.com>
2012-04-23 14:38:00 +0200

    Worked further on Axon Class.

--- src/de/thm/mni/nn/perceptron/impl/Axon.java
+++ src/de/thm/mni/nn/perceptron/impl/Axon.java
@@ -1,30 +1,57 @@
 package de.thm.mni.nn.perceptron.impl;
 
-/*
- * The Axon Class represents the Connections between two Neurons by implementing Source
- * and Target. Training Operations are defined in this class.
+/**
+ * Axon Representation for a Neuronal Network.
+ * Training is implemented inside of this class.
+ * @author Marcel Walden
+ *
  */
 public class Axon {
-	/*
+	/**
 	 * Source Neuron of this Axon
 	 */
 	private Neuron Source;
 	
-	/*
-	 * Target Neuron of this Axon
+	/**
+	 * Target Neuron of the Axon
 	 */
 	private Neuron Target;
 	
-	/*
+	/**
 	 * Actual Weight of the Axon
 	 */
 	private double weight;
 	
-	/*
+	/**
 	 * Through Training actualized Weight of the Axon
 	 */
 	private double weight_new;
 	
+	//CAPSULATION METHODS
+	
+	/**
+	 * Returns the actual Weight of the Axon.
+	 * @return Weight of the Axon.
+	 */
+	private double getWeight() {
+		return weight;
+	}
+	
+/**
+ * Sets the acutal Weight of the Neuron
+ * @param weight Weight to set.
+ */
+	private void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	// CONSTRUCTORS
+	
+	/**
+	 * Constructor requiring two Neurons.
+	 * @param Start Source-Neuron of the Axon.
+	 * @param End Target-Neuron of the Axon.
+	 */
 	public Axon(Neuron Start, Neuron End){
 		this.Source = Start;
 		this.Target = End;
