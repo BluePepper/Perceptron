8b864379b31470f2ab88d9d9b7460e2c3b54acaf
Marcel Walden <Marcel.Walden@me.com>
2012-04-23 14:24:07 +0200

    Axon Definition.

--- src/de/thm/mni/nn/perceptron/impl/Axon.java
+++ src/de/thm/mni/nn/perceptron/impl/Axon.java
@@ -1,5 +1,33 @@
 package de.thm.mni.nn.perceptron.impl;
 
+/*
+ * The Axon Class represents the Connections between two Neurons by implementing Source
+ * and Target. Training Operations are defined in this class.
+ */
 public class Axon {
-
+	/*
+	 * Source Neuron of this Axon
+	 */
+	private Neuron Source;
+	
+	/*
+	 * Target Neuron of this Axon
+	 */
+	private Neuron Target;
+	
+	/*
+	 * Actual Weight of the Axon
+	 */
+	private double weight;
+	
+	/*
+	 * Through Training actualized Weight of the Axon
+	 */
+	private double weight_new;
+	
+	public Axon(Neuron Start, Neuron End){
+		this.Source = Start;
+		this.Target = End;
+	}
+	
 }
--- src/de/thm/mni/nn/perceptron/impl/Neuron.java
+++ src/de/thm/mni/nn/perceptron/impl/Neuron.java
@@ -4,4 +4,7 @@ package de.thm.mni.nn.perceptron.impl;
 
 public class Neuron {
 
+
+	
+	
 }
