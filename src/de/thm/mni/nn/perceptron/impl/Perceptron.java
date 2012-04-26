a62feeba50773a3cb9c583ac920a0c648a1c759b
Alexander Schulz <alexander.schulz@offline-mail.de>
2012-04-26 15:04:02 +0200

    perceptron implemented

--- src/de/thm/mni/nn/perceptron/impl/Perceptron.java
+++ src/de/thm/mni/nn/perceptron/impl/Perceptron.java
@@ -4,8 +4,31 @@ package de.thm.mni.nn.perceptron.impl;
 /**
  * 
  * @author Alexander Schulz
+ * The Perceptron is a collection of Neurons and Axons.
+ * The Perceptron also knows the sort of the Neurons and is able to start a propagate and/or learning.
  *
  */
 public class Perceptron {
+	Neuron[][] neurons;
+	
+	/**
+	 * Standard Constructor
+	 * @param layers The number of Layers
+	 */
+	public Perceptron(Integer layers) {
+		neurons = new Neuron[layers][];
+	}
+	
+	public void addNeuron(Integer layer, Integer count, ENeuronType type, EActivationFunction activationFunction) {
+		if (layer >= neurons.length) {
+			throw new IndexOutOfBoundsException("This Perceptron stores only " + (this.neurons.length-1) + " Layers!");
+		}
+		for (int i = 1; i < count; i++) {
+			Integer actElementCount = this.neurons[layer].length;
+			this.neurons[layer][(actElementCount-1)+i] = new Neuron(activationFunction, type, layer);
+		}
+	}
+	
+	
 
 }
