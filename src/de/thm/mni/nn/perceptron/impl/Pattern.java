ac01176ba603671e19f870da9314aaa104d23152
Alexander Schulz <alexander.schulz@offline-mail.de>
2012-05-02 19:11:35 +0200

    Pattern: Javadoc added

--- src/de/thm/mni/nn/perceptron/impl/Pattern.java
+++ src/de/thm/mni/nn/perceptron/impl/Pattern.java
@@ -18,22 +18,44 @@ public class Pattern {
 	private Boolean[] outputNeurons;
 	private Boolean outputSet = false;
 	
+	/**
+	 * Constructs a New Pattern Object with the given Activation Settings
+	 * @param activation This Boolean Array defines which Neuron is set to 1 or to 0
+	 */
 	public Pattern(Boolean[] activation) {
 		this.inputNeurons = activation.clone();
 	}
 	
+	/**
+	 * Adds the optional Output Pattern in the same way the InputPattern was set in the Contructor.
+	 * This is need to use the Pattern for Training
+	 * @param outputPattern This Boolean Array defines which OutputNeuron should be 1 or 0
+	 */
 	public void addOutputPattern(Boolean[] outputPattern) {
 		this.outputNeurons = outputPattern.clone();
 	}
 	
+	/**
+	 * Returns true if ad Output Pattern has been set.
+	 * @return true if there is an Output Pattern
+	 */
 	public Boolean outputReady() {
 		return outputSet;
 	}
 	
+	/**
+	 * Returns the Number of Input Neuron Settings
+	 * @return the number of input Neurons defined in this Pattern
+	 */
 	public Integer getInputLength() {
 		return inputNeurons.length;
 	}
-	
+
+	/**
+	 * Returns the Number of Output Neuron Settings
+	 * @return the number of Output Neurons defined in this Pattern
+	 * @throws IllegalStateException if there is no OutputPattern
+	 */
 	public Integer getOuputLength() throws IllegalStateException {
 		if(outputSet) {
 			return outputNeurons.length;
